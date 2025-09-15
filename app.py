0e90e2fb73a9f3fdb5db28a5f2b77df3e2c681ca
Shafiqur Rahman <52239911+shafiq-sanchy@users.noreply.github.com>
2025-09-16 04:08:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,14 @@ if st.button("🚀 Extract Emails"):
             else:
                 st.markdown(f"**{site}** → No emails found.")
 
-        # CSV download
+        
+        
+        # Notification
+        st.balloons()
+        st.markdown("<br><br>", unsafe_allow_html=True)
+        st.success(f"🎉 Extraction completed! Total emails found: {total_emails_found}")
+
+# CSV download
         if any(len(es) for es in all_results.values()):
             csv_buffer = io.StringIO()
             writer = csv.writer(csv_buffer)
@@ -192,12 +199,9 @@ if st.button("🚀 Extract Emails"):
                 for e in sorted(emails):
                     writer.writerow([site, e, is_email_valid(e) if verify_emails else "Skipped"])
             csv_bytes = csv_buffer.getvalue().encode("utf-8")
-            st.markdown("<br><br>", unsafe_allow_html=True)
+            
             st.download_button("📥 Download all emails (CSV)", csv_bytes, "emails.csv", "text/csv")
         
-        # Notification
-        st.balloons()
-        st.success(f"🎉 Extraction completed! Total emails found: {total_emails_found}")
         st.info("💡 Done by Shafiq Sanchy")
             
         js_code = f"""
